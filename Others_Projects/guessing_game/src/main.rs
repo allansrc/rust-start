28d2546484b0544b9218143e06fe72e8eae94029
Allan Ramos <allansrc@zenMacPro.local>
2020-11-01 17:51:42 -0300

    upgrade

--- Others_Projects/guessing_game/src/main.rs
+++ Others_Projects/guessing_game/src/main.rs
@@ -5,7 +5,7 @@ use std::io;
 fn main() {
     println!("Guess the number!");
     loop {
-        let secret_number = rand::thread_rng().gen_range(1, 100);
+        let secret_number = rand::thread_rng().gen_range(1, 60);
         let mut guess = String::new();
         io::stdin()
             .read_line(&mut guess)
@@ -17,7 +17,10 @@ fn main() {
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => println!("You win!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
         }
     }
 }
